8e517086537761356fce4410ba030711f7ff6563
Rithic Krishna <rk3712@srmist.edu.in>
2026-02-18 10:34:08 +0530

    feat(UC1): Add application entry and welcome message

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        Palindrome Checker App   ");
+        System.out.println("        Version: 1.0             ");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+    }
+}
