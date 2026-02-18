5f6a5e4d1675d6dfcd0bff3305805d2b6e102e28
Rithic Krishna <rk3712@srmist.edu.in>
2026-02-18 10:35:12 +0530

    feat(UC2): Add hardcoded palindrome check

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -2,11 +2,23 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // ================= UC1 =================
         System.out.println("=================================");
         System.out.println("        Palindrome Checker App   ");
         System.out.println("        Version: 1.0             ");
         System.out.println("=================================");
-        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("Welcome to the Palindrome Checker Application!\n");
+
+        // ================= UC2 =================
+        System.out.println("UC2: Hardcoded Palindrome Check");
+
+        String word = "madam";
+
+        if (word.equals(new StringBuilder(word).reverse().toString())) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
 
     }
 }
