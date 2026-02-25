0f475158b98b50a718e049184608ded666fe4045
rithic-k <rk3712@srmist.edu.in>
2026-02-25 08:55:40 +0530

    feat(UC7): Implement Deque-Based optimized palindrome checker

---
+++ UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" UC7: Deque-Based Optimized Palindrome Checker");
+        System.out.println("=================================");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (char ch : input.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
